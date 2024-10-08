3b0ac79e9b2a270a1f60e9001746101cc3b514c2
CassJah <144091819+CassJah@users.noreply.github.com>
2024-10-09 11:35:39 +1300

    moving icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,59 +32,61 @@ const Header = () => {
         </div>
 
         {/* Social Icons with Tooltips */}
-        <div className="social-icons">
-          <div className="tooltip-container">
-            <a href="mailto:j.cassidy@outlook.co.nz" className="icon-circle">
-              <FontAwesomeIcon icon={faEnvelope} />
+        <div className="social-icons-container">
+          <div className="social-icons">
+            <div className="tooltip-container">
+              <a href="mailto:j.cassidy@outlook.co.nz" className="icon-circle">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>
+              <span className="tooltip-text">j.cassidy@outlook.co.nz</span>
+            </div>
+            <div className="tooltip-container">
+              <a href="tel:+641234567" className="icon-circle">
+                <FontAwesomeIcon icon={faPhone} />
+              </a>
+              <span className="tooltip-text">+64 123 4567</span>
+            </div>
+            <a
+              href="https://www.linkedin.com/in/jvmacuto"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon-circle"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
             </a>
-            <span className="tooltip-text">j.cassidy@outlook.co.nz</span>
-          </div>
-          <div className="tooltip-container">
-            <a href="tel:+641234567" className="icon-circle">
-              <FontAwesomeIcon icon={faPhone} />
+            <a
+              href="https://github.com/CassJah"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon-circle"
+            >
+              <FontAwesomeIcon icon={faGithub} />
             </a>
-            <span className="tooltip-text">+64 123 4567</span>
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon-circle"
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon-circle"
+            >
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon-circle"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
           </div>
-          <a
-            href="https://www.linkedin.com/in/jvmacuto"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="icon-circle"
-          >
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-          <a
-            href="https://github.com/CassJah"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="icon-circle"
-          >
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-          <a
-            href="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="icon-circle"
-          >
-            <FontAwesomeIcon icon={faFacebook} />
-          </a>
-          <a
-            href="https://twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="icon-circle"
-          >
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-          <a
-            href="https://instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="icon-circle"
-          >
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
         </div>
       </div>
     </header>
